097d0964c3ede8fe998d153009f04b445707b497
tatsuomi <tatsu20000303@gmail.com>
2020-09-18 22:12:45 +0900

    countの復習

--- recount/ViewController.swift
+++ recount/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var count: UILabel!
+    var number: Int = 0
     override func viewDidLoad() {
+        count.text = String(number)
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func Plus() {
+        number = number + 1
+        count.text = String(number)
+    }
+    @IBAction func Minus() {
+        number = number - 1
+        count.text = String(number)
+    }
+    @IBAction func Ast() {
+        number = number * 2
+        count.text = String(number)
+    }
+    @IBAction func Sla() {
+        number = number / 5
+        count.text = String(number)
+    }
 }
 
